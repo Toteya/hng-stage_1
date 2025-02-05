eb81793ad688362416654bd6b4742a5cfb72cfda
Toteya <111172685+Toteya@users.noreply.github.com>
2025-02-05 15:51:05 +0200

    added bad request error handler;

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,12 +4,24 @@ module app:
 Flask - Numbers Classification API
 """
 from api.v1 import app_bp
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_bp)
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+
+@app.errorhandler(400)
+def bad_request(error=None):
+    """ Handles 400 errors
+    """
+    response = {
+        'number': 'alphabet',
+        'error': True
+    }
+    return jsonify(response), 400
+
 
 if __name__ == '__main__':
     host = '0.0.0.0'

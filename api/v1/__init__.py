386b657f8b5074e7bb76b8b18af797798cc239e6
Toteya <111172685+Toteya@users.noreply.github.com>
2025-02-05 15:51:34 +0200

    correcion: API base url

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -4,6 +4,6 @@ Blueprint for API
 """
 from flask import Blueprint
 
-app_bp = Blueprint('app_bp', __name__, url_prefix='/api/v1')
+app_bp = Blueprint('app_bp', __name__, url_prefix='/api')
 
 from api.v1.numbers import *

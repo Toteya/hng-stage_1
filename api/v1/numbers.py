84bdc211c5dcd9c1c6875a7e625cf363787e6908
Toteya <111172685+Toteya@users.noreply.github.com>
2025-02-05 17:19:05 +0200

    updated endpoint: added more information to response -> 1) number is odd or even, 2) number is perfect or not

--- api/v1/numbers.py
+++ api/v1/numbers.py
@@ -22,8 +22,8 @@ def classify_number():
     response = {
         'number': number,
         'is_prime': is_prime(number),
-        'is_perfect': '',
-        'properties': [],
+        'is_perfect': is_perfect(number),
+        'properties': get_properties(number),
         'digit_sum': get_digit_sum(number),
         'fun_fact': get_fun_fact(number)
     }
@@ -49,13 +49,40 @@ def get_fun_fact(number):
     response = requests.get(url)
     return response.text
 
+
 def is_prime(number):
     """ Checks and returns whether the given number is a prime number or not
     """
     if number < 2:
         return False
 
-    for i in range(2, int(number/2) + 1):
+    for i in range(2, int(number / 2) + 1):
         if number % i == 0:
             return False
     return True
+
+
+def is_perfect(number):
+    """ Checks and returns whether the given number is a Perfect Number:
+    i.e. A positive integer that equals the sum of its proper factors (all its
+    divisors except itself)
+    """
+    # find the list factors of the number
+    factors = [i for i in range(1, int(number / 2) + 1) if number % i == 0]
+
+    if sum(factors) == number:
+        return True
+    return False
+    
+
+def get_properties(number):
+    """ Returns the properties of a number - whether it's odd or even, whether
+    or not it's an Armstrong number.
+    """
+    properties = []
+    if number % 2 == 0:
+        properties.append('even')
+    else:
+        properties.append('odd')
+
+    return properties
